4d1a216b38adcadb46b010989ce1710020d48103
Богдан <brazbogdan@gmail.com>
2025-08-21 22:02:01 +0300

    updated commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,20 @@
 import axios from 'axios';
-import type { QuizData } from '../types/types';
+import type { CreateQuizPayload, QuizDetail, QuizListItem } from '../types/types';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
-export const getQuizzes = () => api.get('/quizzes');
-export const getQuizById = (id: string) => api.get(`/quizzes/${id}`);
+export const getQuizzes = () =>
+  api.get<QuizListItem[]>('/quizzes');
 
 
-export const createQuiz = (data: QuizData) => api.post('/quizzes', data);
-export const deleteQuiz = (id: string) => api.delete(`/quizzes/${id}`)
+export const getQuizById = (id: string) =>
+  api.get<QuizDetail>(`/quizzes/${id}`);
+
+
+export const createQuiz = (data: CreateQuizPayload) =>
+  api.post<QuizDetail>('/quizzes', data);
+
+export const deleteQuiz = (id: string) =>
+  api.delete<void>(`/quizzes/${id}`);
